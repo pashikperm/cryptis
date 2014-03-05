b636f22b4010e676357b17d9b26cf06855eb3d64
pashikperm <webartifex@gmail.com>
2014-03-05 23:47:42 +0600

    redisson + log4j

--- src/main/java/usr/pashik/securd/Runner.java
+++ src/main/java/usr/pashik/securd/Runner.java
@@ -1,6 +1,9 @@
 package usr.pashik.securd;
 
+import org.redisson.Redisson;
 import redis.clients.jedis.Jedis;
+
+import java.util.Set;
 
 /**
  * Created by pashik on 05.03.14 22:52.
@@ -13,7 +16,12 @@ public class Runner {
         Jedis jedis = new Jedis("localhost");
         jedis.set("foo", "bar");
         String value = jedis.get("foo");
+        System.out.println("jedis = " + value);
 
-        System.out.println(value);
+        Redisson redisson = Redisson.create();
+        Set<String> set = redisson.getSet("mySet");
+        set.add("someString");
+        System.out.println("redisson = " + set.iterator().next());
+        redisson.shutdown();
     }
 }
